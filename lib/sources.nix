25b459c808009603362f6dee00fa2399cee7473e
Robert Hensing <robert@roberthensing.nl>
2021-02-21 12:41:02 +0100

    lib.sources.setName: init
    This isn't very new, but makes the functionality discoverable and
    fits the new naming scheme.

--- lib/sources.nix
+++ lib/sources.nix
@@ -12,6 +12,7 @@ let
     tryEval
     ;
   inherit (lib)
+    boolToString
     filter
     getAttr
     isString
@@ -105,6 +106,8 @@ let
       ) // {
         satisfiesSubpathInvariant = src ? satisfiesSubpathInvariant && src.satisfiesSubpathInvariant;
       };
+
+  setName = name: src: cleanSourceWith { inherit name src; };
 
   # Filter sources by a list of regular expressions.
   #
@@ -238,6 +241,7 @@ in {
     sourceByRegex
     sourceFilesBySuffices
 
+    setName
     trace
     ;
 }
